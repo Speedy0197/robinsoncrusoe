0622a0629e4b23e2a76bf48d35f55592b4d74a16
Icecube50 <baier.alexander.99@gmail.com>
2020-02-28 12:40:28 +0100

    Added camera movement scripts

---
+++ RobinsonCrusoe/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 40;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalMovement = Input.GetAxis("Mouse ScrollWheel") * speed * Time.deltaTime;
+        if (transform.position.y < 10 || transform.position.y > 200) verticalMovement = 0;
+
+        transform.Translate(0, verticalMovement, 0);
+    }
+}
---
+++ RobinsonCrusoe/Assets/Scripts/Camera/CameraViewpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookVector = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(lookVector);
+    }
+}
