c3dba6dac90851de3771d7c86d659f8e39bfa512
Flo Zug <post@florian-zug.de>
2020-03-12 11:25:13 +0100

    #12 Popup init + move marker + only building

---
+++ Assets/Scripts/Learning/Action.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Action : MonoBehaviour
+{
+    public GameObject ActionTest;
+
+    void OnMouseDown()
+    {
+        ActionTest.SetActive(true);
+    }
+
+    public void  removePopup()
+    {
+       ActionTest.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Learning/Build.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build : MonoBehaviour
+{
+
+    public GameObject BuildTest;
+    public GameObject BuildPostion_1;
+
+    public void moveMarker()
+    {
+        BuildTest.transform.position = BuildPostion_1.transform.position;
+    }
+}
