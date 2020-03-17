f80b69eba5436fffe0fd40e24acc1cfa927d60aa
Alexander Baier <baier.alexander.99@gmail.com>
2020-03-17 13:15:00 +0100

    Updated UI in GameScene, added ItemCards (with Graphics)

---
+++ Assets/Scripts/Camera/ActivateUIOnStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateUIOnStart : MonoBehaviour
+{
+    public GameObject ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ui.SetActive(true);   
+    }
+}
---
+++ Assets/Scripts/RobinsonCrusoe_Game/Cards/ItemCards/ItemCard_Start.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCard_Start : MonoBehaviour
+{
+    public Material cardFront;
+    public Material cardBack;
+
+    public bool state = false;
+
+    private void OnMouseDown()
+    {
+        state = !state;
+        if (state)
+        {
+            GetComponent<MeshRenderer>().material = cardBack;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material = cardFront;
+        }
+    }
+}
