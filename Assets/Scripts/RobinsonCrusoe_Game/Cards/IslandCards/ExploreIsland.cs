fbab777214938023835297c587fb29e0770d888c
Alexander Baier <baier.alexander.99@gmail.com>
2020-03-18 16:29:32 +0100

    Added all island tiles, added script to explore them

---
+++ Assets/Scripts/RobinsonCrusoe_Game/Cards/IslandCards/ExploreIsland.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Scripts.RobinsonCrusoe_Game.Cards.IslandCards;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExploreIsland : MonoBehaviour
+{
+    public Material islandBackground;
+    public bool isExplored = false;
+
+    private IslandCard_Deck deck;
+    private MeshRenderer mesh;
+    private IIslandCard myCard;
+    private void Start()
+    {
+        deck = FindObjectOfType<IslandCard_Deck>();
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Explore");
+        if (!isExplored)
+        {
+            isExplored = true;
+
+            myCard = deck.Draw();
+            mesh.material = deck.GetMaterialFromID(myCard.GetMaterialNumber());
+        }
+    }
+}
--- Assets/Scripts/RobinsonCrusoe_Game/Cards/IslandCards/IslandCard_Deck.cs
+++ Assets/Scripts/RobinsonCrusoe_Game/Cards/IslandCards/IslandCard_Deck.cs
@@ -8,34 +8,14 @@ using UnityEngine;
 
 public class IslandCard_Deck : MonoBehaviour
 {
-    public GameObject cardPrefab;
     public Material[] CardFaces;
 
-    private static event EventHandler DrawRequest;
-    private static event EventHandler PutRequest;
-
     private List<IIslandCard> islandDeck;
-    private IIslandCard lastDrawnCard;
 
     // Start is called before the first frame update
     void Start()
     {
-        DrawRequest += OnDrawRequest;
-        PutRequest += OnPutRequest;
-
         PlayCards();
-    }
-
-    private void OnPutRequest(object sender, EventArgs e)
-    {
-        var card = sender as IIslandCard;
-        islandDeck.Add(card);
-        DeckActions.Shuffle(islandDeck);
-    }
-
-    private void OnDrawRequest(object sender, EventArgs e)
-    {
-        Draw();
     }
 
     void PlayCards()
@@ -49,25 +29,16 @@ public class IslandCard_Deck : MonoBehaviour
         }
     }
 
-    void Draw()
+    public IIslandCard Draw()
     {
         var card = islandDeck[0];
-        lastDrawnCard = card;
         islandDeck.RemoveAt(0);
 
-        GameObject newCard = Instantiate(cardPrefab, transform);
-        newCard.name = card.ToString();
+        return card;
     }
 
-    public IIslandCard GetDrawnEventClass()
+    public Material GetMaterialFromID(int id)
     {
-        return lastDrawnCard;
-    }
-    public Material GetMaterialFromName(string name)
-    {
-        string[] splitted = name.Split(';');
-        int id = Convert.ToInt32(splitted[1]);
-
         return CardFaces[id];
     }
 
@@ -85,16 +56,8 @@ public class IslandCard_Deck : MonoBehaviour
         newDeck.Add(new IslandCard_Tile07());
         newDeck.Add(new IslandCard_Tile08());
         newDeck.Add(new IslandCard_Tile09());
+        newDeck.Add(new IslandCard_Volcano());
 
         return newDeck;
-    }
-
-    public static void RequestDraw()
-    {
-        DrawRequest?.Invoke(null, new EventArgs());
-    }
-    public static void RequestPut(IIslandCard card)
-    {
-        PutRequest?.Invoke(card, new EventArgs());
     }
 }
