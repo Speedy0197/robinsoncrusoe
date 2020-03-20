d37184516ad87d18890de2b763c01b3fc786e088
Alexander Baier <baier.alexander.99@gmail.com>
2020-03-20 14:28:09 +0100

    Added Debug Start

---
+++ Assets/Scripts/Generic/Debug_Init.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Overlay.MainMenu;
+using Assets.Scripts.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Debug_Init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Party party = new Party();
+        party.cook = new Assets.Scripts.RobinsonCrusoe_Game.Characters.Cook();
+
+        PartyHandler.CreateParty(party, 1);
+        SceneManager.LoadScene("GameScene");
+    }
+}
