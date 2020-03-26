2ebff3fbb475b7289d4156aef93d605b5a9a338b
Alexander Baier <baier.alexander.99@gmail.com>
2020-03-26 19:00:08 +0100

    Added prefabs for all phases

---
+++ Assets/Scripts/RobinsonCrusoe_Game/RoundSystem/Phases/Actionphase.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.RobinsonCrusoe_Game.RoundSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actionphase : MonoBehaviour
+{
+    public GameObject actionphase_Prefab;
+    private PhaseView myView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myView = GetComponent<PhaseView>();
+        myView.currentPhaseChanged += OnPhaseChange;
+    }
+
+    private void OnPhaseChange(object sender, EventArgs e)
+    {
+        if (myView.currentPhase == E_Phase.Action)
+        {
+            Debug.Log("Entered Phase: Event ");
+            //Show PopUp
+            var ui = FindObjectOfType<GetUIBase>().GetUI();
+            Instantiate(actionphase_Prefab, ui.transform);
+        }
+    }
+}
