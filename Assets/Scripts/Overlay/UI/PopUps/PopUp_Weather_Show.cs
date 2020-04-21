fca8702321bbf1c2e0df4420e2d5a9577634e965
Flo <post@florian-zug.de>
2020-04-22 01:26:21 +0200

    finished Weatherphase

--- Assets/Scripts/Overlay/UI/PopUps/PopUp_Weather_Show.cs
+++ Assets/Scripts/Overlay/UI/PopUps/PopUp_Weather_Show.cs
@@ -1,4 +1,8 @@
 ﻿using Assets.Scripts.Generic.Dice;
+using Assets.Scripts.Player;
+using Assets.Scripts.RobinsonCrusoe_Game.Characters;
+using Assets.Scripts.RobinsonCrusoe_Game.GameAttributes;
+using Assets.Scripts.RobinsonCrusoe_Game.GameAttributes.Food;
 using Assets.Scripts.RobinsonCrusoe_Game.RoundSystem;
 using System;
 using System.Collections;
@@ -14,6 +18,10 @@ public class PopUp_Weather_Show : MonoBehaviour
     public GameObject diceObject_2;
     public RawImage dice_3;
     public GameObject diceObject_3;
+    public GameObject foodDice_1;
+    public GameObject woodDice_1;
+    public GameObject textFoodDice_1;
+    public GameObject textWoodDice_1;
     public Text descriptionText;
 
     public Texture2D[] orangeDiceTexture;
@@ -22,11 +30,31 @@ public class PopUp_Weather_Show : MonoBehaviour
 
     public Button confirm;
     public GameObject popUp;
+    private DiceSet weatherDice;
+
+    private int amountFoodGone = 0;
+    private int amountWoodGone = 0;
     // Start is called before the first frame update
     void Start()
     {
         confirm.onClick.AddListener(TaskOnClick);
-        var weatherDice = RoundSystem.instance.GetWeatherDices();
+        /*if (WeatherContainer not empty){
+         *  if (token == snow)
+         *  {
+         *      GetRidOfSnow(1);
+         *  }
+         *  if (token == storm)
+         *  {
+         *      Wall.DecreaseBy(1);
+         *  }
+         *  if (token == rain)
+         *  {
+         *      GetRidOfRain(1);
+         *  }
+         * 
+         * }
+         */
+        weatherDice = RoundSystem.instance.GetWeatherDices();
         RollDices(weatherDice);
         ShowText(weatherDice);
     }
@@ -39,12 +67,11 @@ public class PopUp_Weather_Show : MonoBehaviour
             return;
         }
         descriptionText.text = "Über der Insel braut sich ein Sturm zusammen";
+        ShowDamage();
     }
 
     private void TaskOnClick()
     {
-        //TODO: interpret results
-
         Destroy(popUp);
         var phaseView = FindObjectOfType<PhaseView>();
         phaseView.NextPhase();
@@ -59,29 +86,99 @@ public class PopUp_Weather_Show : MonoBehaviour
         {
             diceObject_1.SetActive(true);
             var result = WeatherDice_Simulation.RollOrangeWeather();
-            if (result == WeatherDice_Normal.Rain) dice_1.texture = orangeDiceTexture[0];
-            if (result == WeatherDice_Normal.StrongRain) dice_1.texture = orangeDiceTexture[1];
-            if (result == WeatherDice_Normal.Snow) dice_1.texture = orangeDiceTexture[2];
+            if (result == WeatherDice_Normal.Rain)
+            {
+                dice_1.texture = orangeDiceTexture[0];
+                GetRidOfRain(1);
+            }
+            if (result == WeatherDice_Normal.StrongRain)
+            {
+                dice_1.texture = orangeDiceTexture[1];
+                GetRidOfRain(2);
+            }
+            if (result == WeatherDice_Normal.Snow)
+            {
+                dice_1.texture = orangeDiceTexture[2];
+                GetRidOfSnow(1);
+            }
             result_Normal = result;
         }
         if (weatherDice.weatherDice_Advanced)
         {
             diceObject_2.SetActive(true);
             var result = WeatherDice_Simulation.RollWhiteWeather();
-            if (result == WeatherDice_Advanced.StrongRain) dice_2.texture = whiteDiceTexture[0];
-            if (result == WeatherDice_Advanced.Snow) dice_2.texture = whiteDiceTexture[1];
-            if (result == WeatherDice_Advanced.StrongSnow) dice_2.texture = whiteDiceTexture[2];
+            if (result == WeatherDice_Advanced.StrongRain)
+            {
+                dice_2.texture = whiteDiceTexture[0];
+                GetRidOfRain(2);
+            }
+            if (result == WeatherDice_Advanced.Snow)
+            {
+                dice_2.texture = whiteDiceTexture[1];
+                GetRidOfSnow(1);
+            }
+            if (result == WeatherDice_Advanced.StrongSnow) 
+            { 
+                dice_2.texture = whiteDiceTexture[2];
+                GetRidOfSnow(2);
+            }
             result_Advanced = result;
         }
         if (weatherDice.environmentalDice)
         {
             diceObject_3.SetActive(true);
             var result = WeatherDice_Simulation.RollEnvironment();
-            if (result == EnvironmentalDice.Animal) dice_3.texture = redDiceTexture[0];
+            if (result == EnvironmentalDice.Animal)
+            {
+                dice_3.texture = redDiceTexture[0];
+                if(WeaponPower.currentWeaponPower < 3)
+                {
+                    var activeChar = PartyActions.GetActiveCharacter();
+                    int difference = 3 - WeaponPower.currentWeaponPower;
+                    CharacterActions.DamageCharacterBy(difference, activeChar);
+                }
+            }
             if (result == EnvironmentalDice.Nothing) dice_3.texture = redDiceTexture[1];
-            if (result == EnvironmentalDice.FoodLoss) dice_3.texture = redDiceTexture[2];
-            if (result == EnvironmentalDice.PalisadeDamage) dice_3.texture = redDiceTexture[3];
+            if (result == EnvironmentalDice.FoodLoss)
+            {
+                dice_3.texture = redDiceTexture[2];
+                amountFoodGone -= 1;
+                PerishableFood.DecreaseBy(1);
+            }
+            if (result == EnvironmentalDice.PalisadeDamage)
+            {
+                dice_3.texture = redDiceTexture[3];
+                Wall.DowngradeWallBy(1);
+            }
             result_Environment = result;
         }
+    }
+
+    private void GetRidOfSnow(int amount)
+    {
+        amountWoodGone -= amount;
+        Wood.DecreaseWoodBy(amount);
+        GetRidOfRain(amount);
+    }
+
+    private void GetRidOfRain(int amount)
+    {
+        if (amount > Roof.RoofState)
+        {
+            int difference = amount - Roof.RoofState;
+            amountFoodGone -= difference;
+            amountWoodGone -= difference;
+            PerishableFood.DecreaseBy(difference);
+            Wood.DecreaseWoodBy(difference);
+        }
+    }
+    private void ShowDamage()
+    {
+        textFoodDice_1.SetActive(true);
+        textFoodDice_1.GetComponent<Text>().text = amountFoodGone.ToString();
+        foodDice_1.SetActive(true);
+        textWoodDice_1.SetActive(true);
+        textWoodDice_1.GetComponent<Text>().text = amountWoodGone.ToString();
+        woodDice_1.SetActive(true);
     }
 }
