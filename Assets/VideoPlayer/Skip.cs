4700b36e37ac41a29257bcbd9bb15e7f0d46bbfc
Flo <post@florian-zug.de>
2020-04-22 13:39:56 +0200

    Added skipable VideoPlayer

---
+++ Assets/VideoPlayer/Skip.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Skip : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.anyKey) SkipMovie();
+    }
+
+    private void SkipMovie()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
